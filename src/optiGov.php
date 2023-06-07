2390abe346313be1a501ced4a919cab84ad9884a
Julius Türich <info@joonlabs.com>
2023-06-07 10:56:11 +0200

    add first class in `src`

---
+++ src/optiGov.php
@@ -0,0 +1,12 @@
+<?php
+
+class optiGov {
+    /**
+     * Get the version of the library.
+     *
+     * @return string
+     */
+    public static function getVersion() {
+        return '1.0.0';
+    }
+}
