8c56e8f80779004ac42234f5fd68fcba479cf3d4
Julius Türich <info@joonlabs.com>
2023-06-09 00:28:14 +0200

    add `OptiGov::renderWidget(...)` as entry point

---
+++ src/OptiGov.php
@@ -0,0 +1,38 @@
+<?php
+
+class OptiGov
+{
+
+    /**
+     * Renders the widget if request is coming from a user. If the request is coming from a bot, return pre-rendered
+     * and static HTML code for SEO reasons.
+     *
+     * @param string $path The path in the URL that should be used to render the widget.
+     * @param string|array $config The path to the config file or the config itself.
+     * @return void
+     * @throws Exception
+     */
+    public static function renderWidget(string $path, string|array $config = "./optiGov.json"): void
+    {
+        // ensure that the path starts with a slash and does not end with a slash
+        $path = '/' . trim($path, '/');
+
+        // get base directory of the library
+        $baseDirectory = dirname(__FILE__);
+
+        // handle .htaccess creation
+        OptiGovHtaccessProvider::createIfNotExists($baseDirectory);
+
+        // load the config
+        $config = OptiGovConfigProvider::load($baseDirectory, $config, $path);
+
+        // check if the request is coming from a bot
+        if(!OptiGovBotDetector::isRequestFromBot()){
+            // request is coming from a user, render the widget
+            OptiGovWidgetRenderer::render($config);
+        }else{
+            // request is coming from a bot, render the static HTML code
+            OptiGovStaticHtmlRenderer::render($config);
+        }
+    }
+}
