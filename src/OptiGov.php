98fa09cb6a0ad50b21f6744501fe468749413139
Julius Türich <info@joonlabs.com>
2023-06-09 00:35:43 +0200

    add support for rendering a single component

--- src/OptiGov.php
+++ src/OptiGov.php
@@ -35,4 +35,31 @@ class OptiGov
             OptiGovStaticHtmlRenderer::render($config);
         }
     }
+
+    /**
+     * Renders a single component of the widget.
+     *
+     * @param string $path The path in the URL that should be used to render the widget.
+     * @param string|array $config The path to the config file or the config itself.
+     * @param string $component The name of the component to render.
+     * @param array $properties The properties to pass to the component.
+     * @return void
+     * @throws Exception
+     */
+    public static function renderComponent(string $path, string|array $config = "./optiGov.json", string $component = "suche", array $properties = []): void {
+        // ensure that the path starts with a slash and does not end with a slash
+        $path = '/' . trim($path, '/');
+
+        // get base directory of the library
+        $baseDirectory = dirname(__FILE__);
+
+        // load the config
+        $config = OptiGovConfigProvider::load($baseDirectory, $config, $path);
+
+        // check if the request is coming from a bot
+        if(!OptiGovBotDetector::isRequestFromBot()){
+            // request is coming from a user, render the widget
+            OptiGovWidgetRenderer::renderComponent($config, $component, $properties);
+        }
+    }
 }
--- src/OptiGovWidgetRenderer.php
+++ src/OptiGovWidgetRenderer.php
@@ -33,4 +33,45 @@ EOT;
         // render the widget
         print $html;
     }
+
+    /**
+     * Renders a single component of the widget.
+     *
+     * @param OptiGovConfig $config
+     * @param string $component
+     * @param array $properties
+     * @return void
+     */
+    public static function renderComponent(OptiGovConfig $config, string $component, array $properties): void {
+        // get the installation path
+        $installation = $config->getInstallation();
+
+        // add the component to the properties
+        $properties = array_merge_recursive($properties, ["mode" => $component]);
+
+        // convert the properties to JSON
+        $properties = json_encode($properties);
+
+        // add the widget container
+        $html = <<<EOT
+<!-- optiGov Widget -->
+<div id="app"></div>
+EOT;
+
+        // add the widget style
+        $html .= <<<EOT
+<link rel="stylesheet" href="$installation/widget/style.css"/>
+EOT;
+
+        // add the widget script
+        $html .= <<<EOT
+<script type="module">    
+import * as optiGov from "$installation/widget/optiGov.mjs"
+optiGov.renderComponent({rootContainer: "#app",properties: {$properties},configuration: {$config->toJson()}})
+</script>
+EOT;
+
+        // render the widget
+        print $html;
+    }
 }
