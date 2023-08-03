4e90cfb3e3aae9f4f7d1a6efb73595f95610bb01
Julius Türich <info@joonlabs.com>
2023-08-03 15:24:57 +0200

    add demo website

---
+++ demo/optistadt.de/de/buergerservice/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Bürgerservice</title>
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+
+<nav>
+    <ul>
+        <li><a href="#">Startseite</a></li>
+        <li><a href="#">Bürgerservice</a></li>
+        <li><a href="#">Kontakt</a></li>
+    </ul>
+</nav>
+
+<main>
+    <div class="content">
+        <!-- optiGov -->
+
+    </div>
+</main>
+
+<footer>
+    <p>&copy; 2023</p>
+</footer>
+
+</body>
+</html>
