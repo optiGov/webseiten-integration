7d1a093a219c29eff9cfc52cef36709a01c98a00
Julius Türich <info@joonlabs.com>
2023-08-03 15:51:56 +0200

    update `buergerservice/index.php` site

--- demo/optistadt.de/de/buergerservice/index.php
+++ demo/optistadt.de/de/buergerservice/index.php
@@ -3,22 +3,25 @@
 <head>
     <meta charset="UTF-8">
     <title>Bürgerservice</title>
-    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="/css/style.css">
 </head>
 <body>
 
 <nav>
     <ul>
         <li><a href="#">Startseite</a></li>
-        <li><a href="#">Bürgerservice</a></li>
-        <li><a href="#">Kontakt</a></li>
+        <li><a href="/de/buergerservice">Bürgerservice</a></li>
+        <li><a href="/de/schwimmbad">Schwimmbad</a></li>
     </ul>
 </nav>
 
 <main>
     <div class="content">
         <!-- optiGov -->
-
+        <?php
+        require_once __DIR__ . '/optiGov.php';
+        optiGov::renderWidget('/de/buergerservice');
+        ?>
     </div>
 </main>
 
